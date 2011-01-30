d3e7023a4a390899d16e078e31864f76f08c428b
Rory Geoghegan <r.geoghegan@gmail.com>
2011-01-30 15:48:41 +0000

    setup

--- test_dblayer.py
+++ test_dblayer.py
@@ -1,9 +1,32 @@
 import unittest
 import dblayer
+import sqlite3
+import os
+
+TEST_DB_NAME = "test_db.sqlite"
 
 class TestDbLayer(unittest.TestCase):
+    def setUp(self):
+        db = sqlite3.connect(TEST_DB_NAME)
+        with db as cursor:
+            cursor.execute("""
+                CREATE TABLE widgets (
+                    length REAL
+                );""")
+            for l in range(1,11) + [12]:
+                cursor.execute("""
+                    INSERT INTO widgets (length)
+                        VALUES (?);
+                    """, (l,)
+                )
+            cursor.commit()
+        db.close()
+
+    def tearDown(self):
+        os.remove(TEST_DB_NAME)
+    
     def test_average_length(self):
-        db = dblayer.DBLayer("test.sqlite")
+        db = dblayer.DBLayer(TEST_DB_NAME)
 
         self.assertAlmostEqual(
             6.090909, db.average_length(),
@@ -11,7 +34,7 @@ class TestDbLayer(unittest.TestCase):
         )
 
     def test_insert_widget(self):
-        db = dblayer.DBLayer("temp_test.sqlite")
+        db = dblayer.DBLayer(TEST_DB_NAME)
         w = dblayer.Widget(20)
         db.insert_widget(w)
         self.assertEqual(
@@ -19,7 +42,7 @@ class TestDbLayer(unittest.TestCase):
         )
 
     def test_report(self):
-        db = dblayer.DBLayer("test.sqlite")
+        db = dblayer.DBLayer(TEST_DB_NAME)
         self.assertMultiLineEqual("""
 Consolidated Widget Report
 --------------------------
