8ec6920ef12a9ce0d04ce200f9c47ed6f0099e75
Rory Geoghegan <r.geoghegan@gmail.com>
2011-02-07 21:48:43 +0000

    Adding documentation, presentation

--- create.sql
+++ create.sql
@@ -1,4 +1,15 @@
+DROP TABLE widgets;
 CREATE TABLE widgets (
     length REAL
 );
-    
+INSERT INTO widgets (length) VALUES (1.0);
+INSERT INTO widgets (length) VALUES (2.0);
+INSERT INTO widgets (length) VALUES (3.0);
+INSERT INTO widgets (length) VALUES (4.0);
+INSERT INTO widgets (length) VALUES (5.0);
+INSERT INTO widgets (length) VALUES (6.0);
+INSERT INTO widgets (length) VALUES (7.0);
+INSERT INTO widgets (length) VALUES (8.0);
+INSERT INTO widgets (length) VALUES (9.0);
+INSERT INTO widgets (length) VALUES (10.0);
+INSERT INTO widgets (length) VALUES (12.0);
